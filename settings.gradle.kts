c3fc8fefaca6ee4e85bd610cd2eb49b9d1323416
Lubix5778 <mazalubi442@gmail.com>
2026-08-28 09:10:53 -0300

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Win10-Mobile-VM"
+
+include(":app")
